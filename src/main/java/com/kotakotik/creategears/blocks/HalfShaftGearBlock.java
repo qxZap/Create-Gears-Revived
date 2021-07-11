f4e9f6d238dea06f7872be4bc860129d7e087927
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-11 09:27:40 +0200

    might work might not idk gotta go

--- src/main/java/com/kotakotik/creategears/blocks/HalfShaftGearBlock.java
+++ src/main/java/com/kotakotik/creategears/blocks/HalfShaftGearBlock.java
@@ -1,9 +1,15 @@
 package com.kotakotik.creategears.blocks;
 
 import com.kotakotik.creategears.util.ShapeBuilder;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.contraptions.base.IRotate;
+import com.simibubi.create.content.contraptions.base.KineticBlock;
+import com.simibubi.create.content.contraptions.relays.advanced.SpeedControllerBlock;
+import com.simibubi.create.content.contraptions.relays.elementary.ICogWheel;
 import com.simibubi.create.foundation.utility.VoxelShaper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
@@ -12,6 +18,7 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
 
 public class HalfShaftGearBlock extends GearBlock {
     public VoxelShape shape = cuboid(2.0D, 6.0D, 2.0D, 14.0D, 10.0D, 14.0D);
@@ -38,6 +45,46 @@ public class HalfShaftGearBlock extends GearBlock {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return (isLargeCog() ? shaperLarge : shaper).get(state.get(BlockStateProperties.FACING));
+        return super.getShape(state, worldIn, pos, context);
+//        return (isLargeCog() ? shaperLarge : shaper).get(state.get(BlockStateProperties.FACING));
+    }
+
+    public Direction rotateFaceTo(Direction.Axis axis, Direction dir) {
+        // TODO: this sucks
+        if(axis == dir.getAxis()) return dir;
+        switch (axis) {
+            case X: return Direction.NORTH;
+            case Y: return Direction.UP;
+            case Z: return Direction.EAST;
+        }
+        return dir;
+    }
+
+    public Direction getDirectionForPlacement(BlockItemUseContext context) {
+        if (context.getPlayer() != null && context.getPlayer().isSneaking()) {
+            return context.getFace();
+        } else {
+            World world = context.getWorld();
+            BlockState stateBelow = world.getBlockState(context.getPos().down());
+            Direction f = context.getFace().getOpposite();
+            if (AllBlocks.ROTATION_SPEED_CONTROLLER.has(stateBelow) && this.isLargeCog()) {
+                return rotateFaceTo(stateBelow.get(SpeedControllerBlock.HORIZONTAL_AXIS), f);
+            } else {
+                BlockPos placedOnPos = context.getPos().offset(context.getFace().getOpposite());
+                BlockState placedAgainst = world.getBlockState(placedOnPos);
+                Block block = placedAgainst.getBlock();
+                if (ICogWheel.isSmallCog(placedAgainst)) {
+                    return rotateFaceTo(((IRotate)block).getRotationAxis(placedAgainst), f);
+                } else {
+                    Direction.Axis preferredAxis = getPreferredAxis(context);
+                    return rotateFaceTo(preferredAxis != null ? preferredAxis : context.getFace().getAxis(), f);
+                }
+            }
+        }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return getStateForPlacement(context).with(BlockStateProperties.FACING, this.getDirectionForPlacement(context));
     }
 }
