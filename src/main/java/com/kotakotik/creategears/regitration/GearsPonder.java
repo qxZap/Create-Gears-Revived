8abafe94908465d91c0e3b74e5cc78cbd7e13bc9
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-10-24 02:21:27 +0300

    update to the new create version
    aaaaaaaAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/com/kotakotik/creategears/regitration/GearsPonder.java
+++ src/main/java/com/kotakotik/creategears/regitration/GearsPonder.java
@@ -1,6 +1,7 @@
 package com.kotakotik.creategears.regitration;
 
-import com.kotakotik.creategears.Gears;
+import com.simibubi.create.Create;
+import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 import com.simibubi.create.foundation.ponder.PonderRegistry;
 import com.simibubi.create.foundation.ponder.content.ChainDriveScenes;
 import com.simibubi.create.foundation.ponder.content.KineticsScenes;
@@ -8,25 +9,15 @@ import com.simibubi.create.foundation.ponder.content.PonderTag;
 
 public class GearsPonder {
     public static void register() {
-        PonderRegistry.startRegistration(Gears.modid);
-        // i gotta go sleep
-        // but what if i forget what i was doing when i wake up
-        // nah better finish this
+        PonderRegistrationHelper h = new PonderRegistrationHelper(Create.ID);
 
-
-        // please kill me
-
-        // restarting minecraft is so tedious
-        // i just wanna sleep
-
-        PonderRegistry.forComponents(GearsBlocks.GEAR, GearsBlocks.LARGE_GEAR)
+        h.forComponents(GearsBlocks.GEAR, GearsBlocks.LARGE_GEAR)
                 .addStoryBoard("cog/small", KineticsScenes::cogAsRelay)
                 .addStoryBoard("cog/large", KineticsScenes::largeCogAsRelay);
-
-        PonderRegistry.forComponents(GearsBlocks.FULLY_ENCASED_CHAIN_DRIVE)
+        h.forComponents(GearsBlocks.FULLY_ENCASED_CHAIN_DRIVE)
                 .addStoryBoard("chain_drive/relay", ChainDriveScenes::chainDriveAsRelay);
 
-        PonderRegistry.forComponents(GearsBlocks.SIMPLE_GEARSHIFT)
+        h.forComponents(GearsBlocks.SIMPLE_GEARSHIFT)
                 .addStoryBoard("gearshift", KineticsScenes::gearshift);
 
         PonderRegistry.TAGS.forTag(PonderTag.KINETIC_RELAYS)
@@ -34,7 +25,5 @@ public class GearsPonder {
                 .add(GearsBlocks.LARGE_GEAR)
                 .add(GearsBlocks.FULLY_ENCASED_CHAIN_DRIVE)
                 .add(GearsBlocks.SIMPLE_GEARSHIFT);
-
-        PonderRegistry.endRegistration();
     }
 }
